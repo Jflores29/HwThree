ad68f68b80839a8bd90aedd87cb121c46dd76f9a
Javier <jflores71814@yahoo.com>
2016-12-16 13:33:37 -0500

    first commit

---
+++ app/src/main/java/com/javier/hwthree/MainActivity.java
@@ -0,0 +1,15 @@
+package com.javier.hwthree;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// This is application just has a different launcher icon
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
